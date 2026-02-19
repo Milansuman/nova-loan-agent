87ee1c32bea481aed8b1eb13693ecec4182f2bbb
Milansuman <milansuman2016@gmail.com>
2026-02-19 17:42:46 +0530

    fix script

--- run-simulation.ps1
+++ run-simulation.ps1
@@ -1,2 +1,2 @@
 echo "Running simulation"
-Invoke-RestMethod -Method Post -Uri "http://localhost:8000/run-simulation/336d6b14-8a79-4f5a-9701-5bc4f51bc86c"
+Invoke-RestMethod -Method Post -Uri "http://localhost:8000/simulation/336d6b14-8a79-4f5a-9701-5bc4f51bc86c"
