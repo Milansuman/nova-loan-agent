8377964e84f10aa8059290c4959539064757b1dd
Milansuman <milansuman2016@gmail.com>
2026-02-23 09:28:03 +0530

    minor inconsequential edits

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ logging.basicConfig(
 Netra.init(
     app_name="Nova Agent",
     environment=env.ENVIRONMENT,
+    debug_mode=True,
     block_instruments={InstrumentSet.FASTAPI} #type: ignore
 )
 
